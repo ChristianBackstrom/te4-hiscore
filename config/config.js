cded269d70559d0f5ae8fd13c65812c332f0b3e6
ChristianBackstrom <christian.backstrom@elev.ga.ntig.se>
2022-01-27 08:25:39 +0100

    heroku stuff

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || "",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "",
-    host: process.env.DB_HOST || "",
+    username: process.env.User || "",
+    password: process.env.Password || "",
+    database: process.env.Name || "",
+    host: process.env.Host || "",
     dialect: "postgres",
   },
   test: { storage: "database.test.sqlite", dialect: "sqlite" },
